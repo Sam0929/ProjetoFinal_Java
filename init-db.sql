227239d5d946c5032c87f9cfddcea9943345b45b
Sam0929 <vaninisamuel.w@gmail.com>
2023-11-11 09:46:42 -0300

    Docker startup fixes

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,8 @@
 -- init-db.sql
 
 -- Criação de um usuário com todas as permissões
+CREATE DATABASE IF NOT EXISTS mydatabase;
+
 CREATE USER 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
