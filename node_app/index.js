7798f618c12eb9657f55556fa83e7d4932237d1c
Sam0929 <vaninisamuel.w@gmail.com>
2023-11-12 21:42:56 -0300

    Create of CRUD, Structuring the site and improving the organization

--- node_app/index.js
+++ node_app/index.js
@@ -6,17 +6,19 @@ const bodyParser = require('body-parser');
 require('./models/db.js');
 require('./models/User.js');
 
+    const hbs = handlebars.create({
+        
+        defaultLayout: 'main'
+    });
 
-
-
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+    app.engine('handlebars', hbs.engine);
     app.set('view engine', 'handlebars');
 
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
     
     app.use(routes);
-
+    
     app.listen(3000, function () {
         console.log('Servidor rodando na porta 3000');
     });
--- node_app/routes.js
+++ node_app/routes.js
@@ -1,45 +1,65 @@
 const express = require ('express');
 const routes = express.Router();
 const path = require('path');
+const { body, validationResult } = require('express-validator');
 const User = require('./models/User');
 
-routes.use(express.static('public'));
+routes.use(express.static('views'));
 
-routes.get('/a', (req, res) => {
+routes.get('/', (req, res) => {
     console.log('Acessando rota /a');
     res.render('home');
 });
 
+routes.get('/users', (req, res) => {
+    console.log('Acessando rota /users');
+     
+    User.findAll({order: [['id', 'ASC']]}).then(users => {
+        res.render('users', {
+            users: users.map(user => user.toJSON())
+        })
+    });
+});
+
 routes.get('/login', (req, res) => {
     console.log('Acessando rota /login');
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/views/plain-html/login.html'));
 });
 
 routes.get('/cad', (req, res) => {
     console.log('Acessando rota /cad');
     res.render('form');
 });
-
-routes.get('/ola/:nome/:cargo/:cor', function (req, res) {                                      //Quando eu uso /: eu estou dizendo que é um parâmetro         
-    res.send('<h1>Olá ' + req.params.nome + ', seja bem vindo ao meu site!</h1>' 
-    + '<h2>Seu cargo é: ' + req.params.cargo + '</h2>' 
-    + '<h3>Sua cor favorita é: ' + req.params.cor + '</h3>');       
-                    
-});
-
-routes.post('/add', (req, res) => {
-    User.create({
+routes.post('/add',
+    [
+      body('nome').notEmpty().withMessage('O campo Nome é obrigatório'),
+      body('sobrenome').notEmpty().withMessage('O campo Sobrenome é obrigatório'),
+      body('idade').notEmpty().isInt().withMessage('A Idade deve ser um número inteiro'),
+      body('email').notEmpty().isEmail().withMessage('Digite um endereço de e-mail válido'),
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        // Se houver erros de validação, renderize a página do formulário novamente com os erros
+        return res.render('form', { errors: errors.array() });
+      }
+  
+      // Se a validação passar, continue com a lógica de criar o usuário
+      User.create({
         nome: req.body.nome,
         sobrenome: req.body.sobrenome,
         idade: req.body.idade,
-        email: req.body.email
-    }).then(function () {
-        res.redirect('/');
-    }).catch(function (erro) {
-        res.send('Erro: ' + erro);
-    });
-});
-
+        email: req.body.email,
+      })
+        .then(function () {
+          res.redirect('/');
+        })
+        .catch(function (erro) {
+          res.send('Erro: ' + erro);
+        });
+    }
+  );
 
 
 
